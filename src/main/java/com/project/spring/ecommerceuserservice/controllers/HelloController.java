225da87d218650715ac01bd919af94bb318bea6c
Vamsi Krishna <vamsipvk2203@gmail.com>
2024-09-17 15:42:05 +0530

    Initial Commit of User Service

---
+++ src/main/java/com/project/spring/ecommerceuserservice/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.project.spring.ecommerceuserservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
